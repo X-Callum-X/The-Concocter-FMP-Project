02de06264a3bfdb2e989de8b793e6de7660092c4
Callum <ckelly1536@outlook.com>
2026-02-26 17:58:43 +0000

    Hazards
    Added the lava, ice, and rotating fire bar hazards
    Changed the damage system to use an interface, and using that to deal damage appropriately (still needs to be fully used yet)
    Added placeholder textures for the debuff icons
    Added a basic script which rotates the objects it's attached to
    File organization

---
+++ Assets/Programming/Scripts/IDamagable.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public interface IDamagable
+{
+    public void TakeDamage(float damage)
+    {
+       
+    }
+
+    // Update is called once per frame
+    public void Die()
+    {
+        
+    }
+}
