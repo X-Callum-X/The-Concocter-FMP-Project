c30a45e3e5486354e47802afa944a3aff796a0f6
Callum <ckelly1536@outlook.com>
2026-03-04 15:10:06 +0000

    Level 2 Whitebox
    Started the whiteboxing for level 2 (roughly halfway done)
    Tweaked some values
    Added crosses to levels that can't be accessed

--- Assets/Programming/Scripts/Entities/RotateObject.cs
+++ Assets/Programming/Scripts/Entities/RotateObject.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 
 public class RotateObject : MonoBehaviour
 {
+    public bool horizontal;
+    public bool verticalLeft;
+    public bool verticalRight;
+
     private void Update()
     {
-        gameObject.transform.Rotate(gameObject.transform.rotation.x, -0.5f, gameObject.transform.rotation.z);
+        if (horizontal)
+        {
+            gameObject.transform.Rotate(gameObject.transform.rotation.x, -0.5f, gameObject.transform.rotation.z);
+        }
+        
+        if (verticalLeft)
+        {
+            gameObject.transform.Rotate(gameObject.transform.rotation.x, gameObject.transform.rotation.y, -0.5f);
+        }
+
+        if (verticalRight)
+        {
+            gameObject.transform.Rotate(-0.5f, gameObject.transform.rotation.y, gameObject.transform.rotation.z);
+        }
     }
 }
