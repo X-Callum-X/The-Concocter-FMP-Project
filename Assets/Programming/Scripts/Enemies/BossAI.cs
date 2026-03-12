38015c8e2731cb89815be2ded3ba6ff89d68bea6
Callum <ckelly1536@outlook.com>
2026-03-12 17:49:50 +0000

    Boss AI
    Implemented the boss' movement - the boss moves towards the player, and stops when the player is within a certain range
    The laser guns now behave like the electric beams
    Whiteboxed a bit more of level 4

--- Assets/Programming/Scripts/Enemies/BossAI.cs
+++ Assets/Programming/Scripts/Enemies/BossAI.cs
@@ -1,20 +1,77 @@
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem.Android;
 
-public class BossAI : MonoBehaviour
+public class BossAI : MonoBehaviour, IDamagable
 {
+    [Header("References")]
+    private NavMeshAgent agent;
+
+    public LayerMask whatIsPlayer;
+
+    [Header("Variables")]
     public float health;
+    public float maxHealth;
+
     public float damage;
+    public float sightRange;
+
+    public int phaseCount;
 
     public int numberOfEnemiesToSpawn;
 
+    public Transform player;
+
+    private bool playerInAttackRange;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+
+        health = maxHealth;
+    }
+
     private void Update()
     {
+        playerInAttackRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
 
+        if (!playerInAttackRange)
+        {
+            Chase();
+        }
+        else
+        {
+            AttackPhase();
+        }
+
+        if (health > maxHealth)
+        {
+            phaseCount = 1;
+        }
+        else
+        {
+            phaseCount = 2;
+        }
+    }
+
+    private void Chase()
+    {
+        agent.SetDestination(player.position);
     }
 
     private void AttackPhase()
     {
+        agent.SetDestination(transform.position);
 
+        if (phaseCount == 1)
+        {
+
+        }
+        else
+        {
+
+        }
     }
 
     private void PerformAttack()
@@ -27,13 +84,20 @@ public class BossAI : MonoBehaviour
 
     }
 
-    private void TakeDamage()
+    private void TakeDamage(float damageTaken)
     {
-
+        health -= damageTaken;
     }
 
     private void Die()
     {
+        
+    }
 
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
